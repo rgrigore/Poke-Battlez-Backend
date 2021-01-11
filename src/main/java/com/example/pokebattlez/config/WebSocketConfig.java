ef87a2350300d5d6434f705b7b6f4518125b2312
Razvan Grigore <razvang95@gmail.com>
2021-01-11 19:52:31 +0200

    Add missing /

--- src/main/java/com/example/pokebattlez/config/WebSocketConfig.java
+++ src/main/java/com/example/pokebattlez/config/WebSocketConfig.java
@@ -17,6 +17,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("chat-lobby").withSockJS();
+        registry.addEndpoint("/chat-lobby").withSockJS();
     }
 }

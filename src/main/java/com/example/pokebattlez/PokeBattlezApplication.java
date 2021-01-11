84b78116898e1ba0b87098781511e33caaf15a30
Razvan Grigore <razvang95@gmail.com>
2021-01-11 17:05:27 +0200

    Initial commit

---
+++ src/main/java/com/example/pokebattlez/PokeBattlezApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokebattlez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeBattlezApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeBattlezApplication.class, args);
+    }
+
+}

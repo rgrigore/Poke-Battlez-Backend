49607c6a9c74c4028d95d65d7f700fc67fe76813
Marius Ceobanu <ceobanu.marius@gmail.com>
2021-03-03 14:19:44 +0200

    Implement SecurityConfig configuration class PB-125

---
+++ src/main/java/com/example/pokebattlez/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.pokebattlez.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@AllArgsConstructor
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenService jwtTokenService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .httpBasic().disable()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/account/register").permitAll()
+                .antMatchers(HttpMethod.POST, "/account/login").permitAll()
+                .anyRequest().authenticated()
+            .and()
+                .addFilterBefore(new JwtTokenFilter(jwtTokenService), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
